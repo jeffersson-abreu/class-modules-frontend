283f8d3791632a6c0b86e80401c793e9a1dc8477
ctw6av <ctw6av@gmail.com>
2022-02-25 08:52:40 -0400

    Smart fixing

--- src/utils/comunication.js
+++ src/utils/comunication.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.2:8000',
+  baseURL: 'http://127.0.0.1:8000',
 });
 
 // Add a request interceptor to insert user token
